3581d573f150354a11d9a84ac1f36757627e42f5
Sergey Vasilyev <kosh.goo@gmail.com>
2019-12-10 16:13:10 +0300

    update scripts
    ipCheck: проверка ip не через регулярку, а циклами
    reverseWords: проверка символов не регуляркой. Теперь должно работать с любым языком
    sum35: убран последний for, добавлена проверка в первый for

--- functions.js
+++ functions.js
@@ -1,33 +1,75 @@
 (function () {
+  const punctChars = /^([!?.,:;%]?)$/;
+
+  const checkFirst = function (arrayOfChars) {
+    let punctArr = [];
+
+    while (true) {
+      if (arrayOfChars[0] && arrayOfChars[0].match(punctChars)) {
+        punctArr.unshift(arrayOfChars.splice(0, 1)[0]);
+      } else {
+        return arrayOfChars.concat(punctArr);
+      }
+    }
+  };
+
   window.myFunctions = {
     ipCheck: function (str) {
-      const template = /^(0|[1-9][0-9]?|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.(0|[1-9][0-9]?|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.(0|[1-9][0-9]?|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.(0|[1-9][0-9]?|1[0-9][0-9]|2[0-4][0-9]|25[0-5])$/;
-      return template.test(str);
+      const octets = str.split('.');
+      let flag = false;
+
+      if (octets.length === 4) {
+        for (let i = 0; i < octets.length; i++) {
+          let octet = octets[i];
+          if (octet && octet.length <= 3 && !(octet[0] === '0' && octet.length > 1) && parseInt(octet, 10) >= 0 && parseInt(octet, 10) <= 255) {
+            for (let k = 0; k < octet.length; k++) {
+              if (!isNaN(parseInt(octet[k], 10))) {
+                flag = true;
+              } else {
+                flag = false;
+                break;
+              }
+            }
+            if (!flag) break;
+          } else {
+            flag = false;
+            break;
+          }
+        }
+      }
+
+      return flag;
     },
 
     reverseWordsInSentence: function (str) {
-      if (!str) {
-        return 'Insert your sentence'
-      }
+      let words = str.split(' ');
+      let result = '';
 
-      const template = /^([A-Za-zА-Яа-я0-9]?)$/;
-      let newStr = '';
-
-      str.toLowerCase().split(' ').forEach(function (el) {
-        const newArr = [];
-
-        el.split('').forEach(function (letter, index, str) {
-          if (letter.match(template) || str[index + 1] && str[index+1].match(template)) {
-            newArr.unshift(letter);
-          } else {
-            newArr[index] = letter;
-          }
-        });
-
-        newStr = newStr + ' ' + newArr.join('');
+      words = words.map(function (word) {
+        return word.split('').reverse();
       });
 
-      return newStr[1].toUpperCase() + newStr.slice(2);
+      words.forEach(function (word) {
+        if (word[0]) {
+          let isCapital = false;
+
+          if (word[word.length - 1] === word[word.length - 1].toUpperCase()) {
+            isCapital = true;
+            word[word.length - 1] = word[word.length - 1].toLowerCase();
+          }
+
+          word = checkFirst(word);
+
+          if (isCapital) {
+            word[0] = word[0].toUpperCase();
+            isCapital = false;
+          }
+
+          result = result + ' ' + word.join('');
+        }
+      });
+
+      return result;
     },
 
     sumOfMultipleNumbers: function (num) {
@@ -36,15 +78,15 @@
       let sum35 = 0;
 
       for (let i = 3; i < num; i += 3) {
+        if (i % 5 === 0) {
+          continue;
+        }
+
         sum3 += i;
       }
 
       for (let i = 5; i < num; i += 5) {
         sum5 += i;
-      }
-
-      for (let i = 15; i < num; i += 15) {
-        sum35 += i;
       }
 
       return sum3 + sum5 - sum35;
